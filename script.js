770c946978f3e7c7194929de32fd3dd7b0e55a68
Jrevenga06 <jorge.revenga@edu.uah.es>
2023-12-04 02:19:16 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggleButton');
+    const indice = document.getElementById('indice');
+    const contenido = document.getElementById('contenido');
+
+    toggleButton.addEventListener('click', function () {
+        indice.classList.toggle('oculto');
+        toggleButton.classList.toggle('cerrado');
+        contenido.classList.toggle('oculto');
+        toggleButton.classList.toggle('desplazado');
+    });
+
+    // Cambio de color al hacer hover sobre el encabezado
+    const header = document.querySelector('header');
+    header.addEventListener('mouseover', function () {
+        header.style.backgroundColor = '#2980b9';
+    });
+
+    header.addEventListener('mouseout', function () {
+        header.style.backgroundColor = '#3498db';
+    });
+
+    // Desplegar sublistas al hacer clic en los títulos
+    const h2Elements = document.querySelectorAll('#indice h2');
+    h2Elements.forEach(h2 => {
+        h2.addEventListener('click', function () {
+            const ul = this.nextElementSibling;
+            if (ul.style.display === 'none') {
+                ul.style.display = 'block';
+            } else {
+                ul.style.display = 'none';
+            }
+        });
+    });
+
+    // Inicializar ScrollReveal
+    const sr = ScrollReveal({
+        duration: 1000,
+        easing: 'ease',
+        mobile: false,
+    });
+
+    // Animación para el encabezado
+    sr.reveal('header', {
+        origin: 'top',
+        distance: '50px',
+    });
+
+    // Animación para las secciones
+    sr.reveal('section', {
+        origin: 'bottom',
+        distance: '50px',
+        interval: 200,
+    });
+
+    // Desplazamiento suave al hacer clic en los enlaces del índice
+    const links = document.querySelectorAll('#indice a');
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - header.offsetHeight,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
